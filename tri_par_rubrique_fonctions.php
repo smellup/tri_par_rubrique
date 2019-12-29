c37c74f9912b3ef94c18290b5d59b85d9b28abbf
Eric Lupinacci <eric@smellup.net>
2019-12-29 21:42:28 +0100

    Mise au point du critère tri_rubrique pour qsu'il gère le cas où il n'y a pas de rubrique dans l'environnement. On en profite pour simplifier l'écriture pour passer par une fonction plus simple à gérer. Ajout de deux filtres tri_rubrique_champ et tri_rubrique_sens qui renvoient une valeur compatible avec les critères tri, par et inverse. Il est donc possible d'utiliser ces critères plutôt que le tri_rubrique car ceux-ci se dégradent mieux si on enlève le plugin.

--- tri_par_rubrique_fonctions.php
+++ tri_par_rubrique_fonctions.php
@@ -27,8 +27,8 @@ function filtre_tri_par_rubrique_criteres_dist() {
 	);
 	if(
 		test_plugin_actif('rang') 
-		&& function_exists('rang_liste_objets') 
-		&& in_array('articles', rang_liste_objets())
+		and function_exists('rang_liste_objets')
+		and in_array('articles', rang_liste_objets())
 	){
 		$criteres['rang'] = _T('tri_par_rubrique:tri_articles_rang');
 	}
@@ -43,13 +43,87 @@ function filtre_tri_par_rubrique_criteres_dist() {
  * @param $crit
  */
 function critere_tri_rubrique($idb, &$boucles, $crit) {
+
 	$boucle = &$boucles[ $idb ];
 
-	if($boucle->id_table != 'articles'){
-		return (array('zbug_tri_rubrique_sur_articles_uniquement'));
+	// Uniquement les boucles articles sont concernées par le critère de tri.
+	if ($boucle->id_table != 'articles') {
+		return array('zbug_tri_rubrique_sur_articles_uniquement');
 	}
-	$tri = 'sql_getfetsel(\'trirub_articles\',\'spip_rubriques\',\'id_rubrique = \'.intval($Pile[0][\'id_rubrique\']))';
-	$inverse = '(sql_getfetsel(\'trirub_articles_inverse\',\'spip_rubriques\',\'id_rubrique = \'.intval($Pile[0][\'id_rubrique\'])) ? " DESC" : "")';
-	
-	$boucle->order[] = $tri.'.'.$inverse;	
+
+	// Existence d'un collate ou pas
+	$collecte = '';
+	if (isset($boucle->modificateur['collate'])) {
+		$collecte = $boucle->modificateur['collate'];
+	}
+
+	// Détermination du tri configuré pour la rubrique ou globalement par défaut si aucun id_rubrique dans l'environnement
+	$boucle->order[] = 'calculer_tri_rubrique(intval($Pile[0][\'id_rubrique\']), $collecte)';
+}
+
+function calculer_tri_rubrique($id_rubrique, $collecte) {
+
+	$expression_tri = '';
+
+	if ($id = intval($id_rubrique)) {
+		// On a bien un id_rubrique dans l'environnement
+		$champs_tri = array('trirub_articles', 'trirub_articles_inverse');
+		$config_tri = sql_fetsel($champs_tri, 'spip_rubriques', 'id_rubrique=' . $id);
+	} else {
+		// Pas d'id_rubrique, on se rabat sur la config globale
+		include_spip('inc/config');
+		$config_tri = lire_config('tri_par_rubrique', array());
+	}
+
+	if ($config_tri) {
+		// On a bien la config du tri, on peut construire l'expression de tri.
+		// Sinon, on renvoie une expression vide ce qui laisse le tri par défaut de SQL.
+		$tri = $config_tri['trirub_articles'];
+		$sens = $config_tri['trirub_articles_inverse'] ? 'DESC' : 'ASC';
+
+		$expression_tri = "${tri} ${collecte} ${sens}";
+	}
+
+	return $expression_tri;
+}
+
+
+function tri_rubrique_champ($id_rubrique) {
+
+	// On initialise le champ à vide.
+	$champ = '';
+
+	if ($id = intval($id_rubrique)) {
+		// On a bien un id_rubrique dans l'environnement
+		$champ = sql_fetsel('trirub_articles', 'spip_rubriques', 'id_rubrique=' . $id);
+	}
+
+	if (!$champ) {
+		// Pas d'id_rubrique ou erreur SQL sur le fetsel, on se rabat sur la config globale
+		include_spip('inc/config');
+		$champ = lire_config('tri_par_rubrique/trirub_articles');
+	}
+
+	return $champ;
+}
+
+
+
+function tri_rubrique_sens($id_rubrique) {
+
+	// On initialise la variable à false pour la distinguer de 0.
+	$inverse = false;
+
+	if ($id = intval($id_rubrique)) {
+		// On a bien un id_rubrique dans l'environnement
+		$inverse = sql_fetsel('trirub_articles', 'spip_rubriques', 'id_rubrique=' . $id);
+	}
+
+	if ($inverse === false) {
+		// Pas d'id_rubrique ou erreur SQL sur le fetsel, on se rabat sur la config globale
+		include_spip('inc/config');
+		$inverse = lire_config('tri_par_rubrique/trirub_articles_inverse');
+	}
+
+	return $inverse ? -1 : 1;
 }
